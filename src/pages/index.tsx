04ea618e98c18e6df5e5e19c3573d2b071fabb78
kinmod <impratham2001@gmail.com>
2023-05-25 01:21:30 +0530

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
   const user = useUser();
 
   return (
@@ -19,6 +19,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div> {!user.isSignedIn && <SignInButton />}</div>
         <div> {!!user.isSignedIn && <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
