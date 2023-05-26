ad6ac80f74366c0a5be0ca4995de3681fb78fd46
kinmod <impratham2001@gmail.com>
2023-05-26 23:26:42 +0530

    Render posts on profile page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,14 @@ import Head from "next/head";
 import { SignInButton, useUser } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
 
 import { LoadingPage, LoadingSpinner } from "../components/loading";
 
-import dayjs from "dayjs";
-import realtiveTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import Link from "next/link";
 import { PageLayout } from "../components/layout";
-
-dayjs.extend(realtiveTime);
+import { PostView } from "../components/postview";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -79,40 +74,6 @@ const CreatePostWizard = () => {
           <LoadingSpinner size={20} />
         </div>
       )}
-    </div>
-  );
-};
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Link href={`/@${author.username}`}>
-        <Image
-          src={author.profileImageUrl}
-          alt={`@${author.username}'s profile picture`}
-          width={56}
-          height={56}
-          className="h-14 w-14 rounded-full"
-        />
-      </Link>
-      <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username} `}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{`  · ${dayjs(
-              post.createdAt
-            ).fromNow()}`}</span>
-          </Link>
-        </div>
-        <Link href={`/post/${post.id}`}>
-          <span className="text-2xl">{post.content}</span>
-        </Link>
-      </div>
     </div>
   );
 };
